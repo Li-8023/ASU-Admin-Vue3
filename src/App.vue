deec0ccbbb20e5c988954ab3b407bed6a50fe273
Li He <80284104+Li-8023@users.noreply.github.com>
2023-10-09 20:29:23 -0700

    set up

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <!-- Content -->
+    <router-view />
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
