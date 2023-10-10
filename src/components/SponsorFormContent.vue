b0cc8e1505d253ad6752c26b75bb072254987ae4
Li He <80284104+Li-8023@users.noreply.github.com>
2023-10-10 14:35:05 -0700

    add link

---
+++ src/components/SponsorFormContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    SponsorForm Page
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/HomePage.vue";
 import Student from "../components/StudentPageContent.vue";
 import Sponsor from "../components/SponsorPageContent.vue";
+import SponsorForm from "../components/SponsorFormContent.vue"
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path:"/sponsor",
     name:"Sponsor",
     component: Sponsor,
+  },
+  {
+    path:"/sponsorForm",
+    name:"SponsorForm",
+    component: SponsorForm,
   }
 ];
 const router = createRouter({
